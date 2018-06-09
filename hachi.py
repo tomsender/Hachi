8ba5351c572743a99c809704fa77d528fa44f51a
Tom Sender <Tom@sears.co.il>
2018-06-10 01:09:03 +0300

    Fix error

--- hachi.py
+++ hachi.py
@@ -29,7 +29,6 @@ def main():
         last_chat_name = last_update['message']['chat']['first_name']
 
         if last_chat_text.lower() in status:
-            hachi.state
             greet_bot.send_message(last_chat_id, hachi.get_status())
             today += 1
 
