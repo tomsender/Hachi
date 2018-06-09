917e4deae5402e00f323cc0b9559da2d9636bf93
Tom Sender <Tom@sears.co.il>
2018-06-10 00:50:37 +0300

    add hachi statuses

--- hachi.py
+++ hachi.py
@@ -1,9 +1,16 @@
 import datetime
 
 from BotHandler import BotHandler
+from HachiStatuses import StarvingState, HyperactiveState, SatisfiedState
+from HachiStatus import HachiStatus
+hachi = HachiStatus()
+
 
 greet_bot = BotHandler("562742113:AAFzvPJQMsmrYe2K0tdQvViFSqV3R3T1Krw")
 greetings = ('hello', 'hi', 'greetings', 'sup')
+status = ('status', 'hachi')
+ateKeys = ('ate')
+walkedKeys = ('walked, kaki')
 now = datetime.datetime.now()
 
 
@@ -21,6 +28,22 @@ def main():
         last_chat_text = last_update['message']['text']
         last_chat_id = last_update['message']['chat']['id']
         last_chat_name = last_update['message']['chat']['first_name']
+
+        if last_chat_text.lower() in status:
+            hachi.state
+            greet_bot.send_message(last_chat_id, hachi.get_status())
+            today += 1
+
+        if last_chat_text.lower() in ateKeys:
+            HachiStatus.on_event(hachi, ateKeys[0])
+            greet_bot.send_message(last_chat_id, hachi.get_status())
+            today += 1
+
+        if last_chat_text.lower() in walkedKeys:
+            HachiStatus.on_event(hachi, walkedKeys[0])
+            greet_bot.send_message(last_chat_id, hachi.get_status())
+            today += 1
+
 
         if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
             greet_bot.send_message(last_chat_id, 'Good Morning  {}'.format(last_chat_name))
